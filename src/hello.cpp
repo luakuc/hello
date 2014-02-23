dd6109e3d7fa742597921044b7f55745dea45f80
kutty <kutty@163.com>
2014-02-23 14:33:45 -0500

    创建工程

---
+++ src/hello.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	int number ;
+	cin  >> number;
+	cout << "you are "<< number << endl;
+
+	std::cout << number << endl;
+
+	return 0;
+}
